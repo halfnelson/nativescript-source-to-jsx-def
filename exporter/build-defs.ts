6f542cea950d37f62718642835e0234316aad86d
halfnelson <dpershouse@gmail.com>
2020-11-21 23:26:28 +1000

    exporter keeps its output to itself

--- exporter/build-defs.ts
+++ exporter/build-defs.ts
@@ -9,24 +9,24 @@ function exportModule(moduleName: string) {
     let doc = buildModuleJSX(moduleName, modulePath, path.dirname(require.resolve("@nativescript/core/package.json")));
     let module_filename = moduleName.replace(/[^\w]/g, '-');
 
-    fs.writeFileSync(`../svelte-native-defs/svelte-native-jsx-${module_filename}.d.ts`, renderSvelte(doc));
-    fs.writeFileSync(`../react-nativescript-defs/react-nativescript-jsx-${module_filename}.ts`, renderReact(doc));
+    fs.writeFileSync(`./svelte-native-defs/svelte-native-jsx-${module_filename}.d.ts`, renderSvelte(doc));
+    fs.writeFileSync(`./react-nativescript-defs/react-nativescript-jsx-${module_filename}.ts`, renderReact(doc));
 }
 
 function exportCore(corePath: string) {
     console.log(`Exporting Core`)
     let doc = buildCoreJSX(corePath);
 
-    fs.writeFileSync("../svelte-native-defs/svelte-native-jsx-nativescript-core.d.ts", renderSvelte(doc));
-    fs.writeFileSync("../react-nativescript-defs/react-nativescript-jsx.ts", renderReact(doc, true));
+    fs.writeFileSync("./svelte-native-defs/svelte-native-jsx-nativescript-core.d.ts", renderSvelte(doc));
+    fs.writeFileSync("./react-nativescript-defs/react-nativescript-jsx.ts", renderReact(doc, true));
 }
 
 function doExports() {
     const nativescriptSourcePath = path.resolve(__dirname, "./nativescript_src/packages/core");
     chdir(__dirname)
 
-    fs.mkdirSync("../svelte-native-defs", { recursive: true});
-    fs.mkdirSync("../react-nativescript-defs", { recursive: true});
+    fs.mkdirSync("./svelte-native-defs", { recursive: true});
+    fs.mkdirSync("./react-nativescript-defs", { recursive: true});
 
     exportCore(nativescriptSourcePath)
     exportModule("nativescript-ui-listview")
